625527a6cb193c563e179dbe6f6410512b09ee02
Taylor MacDonald <tcmacdonald@gmail.com>
2017-08-21 23:24:34 -0400

    Implement menu template

--- app/features/header.rb
+++ app/features/header.rb
@@ -1,6 +1,7 @@
 AmpleAdmin::FeatureDefinition.define do
   factory :header do
     attribute :body, :text
+    attribute :content_tag, ['H2', 'H3', 'H4']
     attribute :region, nil
   end
 end
--- app/helpers/public/pages_helper.rb
+++ app/helpers/public/pages_helper.rb
@@ -1,2 +1,27 @@
 module Public::PagesHelper
+
+  def current_subnav
+    @current_nav_items ||= begin
+      Rails.cache.fetch([current_object, 'subnav']) do
+        if current_object.has_children?
+          current_object.children
+        else
+          current_object.siblings
+        end
+      end
+    end
+  end
+
+  def current_parent
+    @current_parent ||= begin
+      Rails.cache.fetch([current_object, 'parent']) do
+        if current_object.has_parent?
+          current_object.parent
+        else
+          current_object
+        end
+      end
+    end
+  end
+
 end
