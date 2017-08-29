deaddea1c903bbe3dfcc608daba8209889b4ccee
Sean C Davis <scdavis41@gmail.com>
2017-08-29 10:58:37 -0400

    Styles for menu items and menu groups

--- app/features/menu_item.rb
+++ app/features/menu_item.rb
@@ -2,6 +2,8 @@ AmpleAdmin::FeatureDefinition.define do
   factory :menu_item do
     attribute :region, nil
     attribute :description, :text
+    attribute :vegan, :boolean
+    attribute :gluten_free, :boolean
     attribute :price, :string
   end
 end
