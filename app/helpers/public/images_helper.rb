2d82b22ebaacc5d6d43c4ce72ce4e913bab226b0
Sean C Davis <scdavis41@gmail.com>
2017-09-08 08:24:14 -0400

    Remove comment

--- app/helpers/public/images_helper.rb
+++ app/helpers/public/images_helper.rb
@@ -5,7 +5,6 @@ module Public::ImagesHelper
     classes += (options[:class]).to_s
     content_tag(:svg, class: classes, 'viewBox': '0 0 32 32') do
       href = "#{image_path('icons.svg')}##{name}"
-      # "<use xmlns:xlink=\"http://www.w3.org/1999/xlink\" xlink:href=\"#{href}\" height=\"32\" width=\"32\" />".html_safe
       content_tag(:use, nil, 'xlink:href': href, height: '32', width: '32')
     end
   end
