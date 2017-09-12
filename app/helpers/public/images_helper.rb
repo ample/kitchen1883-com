1c9bcd190abcb8b006aac5cb7238ebd8ad65814f
Sean C Davis <scdavis41@gmail.com>
2017-09-12 16:17:53 -0400

    [#37] Add colors to SVG images

--- app/helpers/public/images_helper.rb
+++ app/helpers/public/images_helper.rb
@@ -1,11 +1,15 @@
 module Public::ImagesHelper
 
+  def icon_path(name)
+    image_path("icons/#{name}")
+  end
+
   def icon(name, options = {})
     classes  = "icon icon-#{name} color-#{options[:color]} #{options[:size]} "
     classes += (options[:class]).to_s
     content_tag(:svg, class: classes, 'viewBox': '0 0 32 32') do
       href = "#{image_path('icons.svg')}##{name}"
-      content_tag(:use, nil, 'xlink:href': href, height: '32', width: '32')
+      content_tag(:use, nil, 'xlink:href': href, 'xmlns:xlink': 'http://www.w3.org/1999/xlink', height: '32', width: '32', 'data-img': icon_path("#{name}.png"))
     end
   end
 
