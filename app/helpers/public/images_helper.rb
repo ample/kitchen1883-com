8cd1f3d19605e2ca42a069a29f5a24750454f5e6
Sean C Davis <scdavis41@gmail.com>
2017-09-12 16:40:03 -0400

    [#37] Fix icons in IE

--- app/helpers/public/images_helper.rb
+++ app/helpers/public/images_helper.rb
@@ -1,15 +1,11 @@
 module Public::ImagesHelper
 
-  def icon_path(name)
-    image_path("icons/#{name}")
-  end
-
   def icon(name, options = {})
-    classes  = "icon icon-#{name} color-#{options[:color]} #{options[:size]} "
+    classes  = "icon icon-#{name} #{options[:size]} "
     classes += (options[:class]).to_s
-    content_tag(:svg, class: classes, 'viewBox': '0 0 32 32') do
+    content_tag(:svg, class: classes, 'viewBox': '0 0 256 256') do
       href = "#{image_path('icons.svg')}##{name}"
-      content_tag(:use, nil, 'xlink:href': href, 'xmlns:xlink': 'http://www.w3.org/1999/xlink', height: '32', width: '32', 'data-img': icon_path("#{name}.png"))
+      content_tag(:use, nil, 'xlink:href': href, 'xmlns:xlink': 'http://www.w3.org/1999/xlink')
     end
   end
 
