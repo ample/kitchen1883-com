642a2a623705f11fce76ac75677e3afb69fe1b93
Taylor MacDonald <tcmacdonald@gmail.com>
2017-11-08 09:50:59 -0500

    Disable gzip compression on sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 SitemapGenerator::Sitemap.default_host = "https://www.kitchen1883.com"
-
+SitemapGenerator::Sitemap.compress = false
 SitemapGenerator::Sitemap.create do
   AmpleAdmin::Page.live.find_each do |page|
     add page.hierarchy_permalink, :lastmod => page.updated_at
