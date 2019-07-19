a2eea64b0129567ba73cb128da58ad09b02d7055
Sean C Davis <scdavis41@gmail.com>
2019-07-19 11:25:59 -0400

    Add image to show that gatsby image is working

--- src/fragments/page.js
+++ src/fragments/page.js
@@ -3,5 +3,10 @@ import { graphql } from "gatsby"
 export const PageAttributes = graphql`
   fragment PageAttributes on ContentfulPage {
     title
+    jumbotronImage: image {
+      fluid(maxWidth: 2400) {
+        ...GatsbyContentfulFluid_withWebp
+      }
+    }
   }
 `
--- src/layout/page.js
+++ src/layout/page.js
@@ -4,14 +4,17 @@ import PropTypes from "prop-types"
 import Layout from "./index"
 
 import Button from "../components/button"
+import Img from "gatsby-image"
 
 class Page extends React.Component {
   render() {
     const { page } = this.props
+    console.log(page)
     return (
       <Layout>
         <p>This is the {page.title} page</p>
         <Button>Hello</Button>
+        <Img fluid={page.jumbotronImage.fluid} />
       </Layout>
     )
   }
