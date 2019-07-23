a88f00ffd5feb054eb2bdec3fe4b45d3d1ef9018
Sean C Davis <scdavis41@gmail.com>
2019-07-23 09:37:04 -0400

    Add layout base notes

--- stories/layout.stories.js
+++ stories/layout.stories.js
@@ -6,30 +6,36 @@ import { withKnobs } from "@storybook/addon-knobs"
 import GlobalStyles from "../src/components/layout/global-styles"
 import Layout from "../src/components/layout"
 
+import notes from "./notes/layout.md"
+
 const stories = storiesOf("Layout", module)
 
 stories.addDecorator(withKnobs)
 
-stories.add("Layout", () => {
-  return (
-    <GlobalStyles>
-      <Layout>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua.
-        </p>
-        <p>
-          Massa enim nec dui nunc mattis enim. Sed pulvinar proin gravida hendrerit. Diam volutpat
-          commodo sed egestas egestas. Eu tincidunt tortor aliquam nulla facilisi cras. Orci
-          sagittis eu volutpat odio facilisis mauris sit amet massa.
-        </p>
-        <p>
-          Lectus sit amet est placerat in egestas erat imperdiet sed. Ipsum a arcu cursus vitae
-          congue mauris rhoncus aenean. Eget egestas purus viverra accumsan in nisl. Eu volutpat
-          odio facilisis mauris sit amet massa vitae tortor. Sit amet purus gravida quis blandit
-          turpis cursus in hac. Vitae tortor condimentum lacinia quis.
-        </p>
-      </Layout>
-    </GlobalStyles>
-  )
-})
+stories.add(
+  "Layout",
+  () => {
+    return (
+      <GlobalStyles>
+        <Layout>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua.
+          </p>
+          <p>
+            Massa enim nec dui nunc mattis enim. Sed pulvinar proin gravida hendrerit. Diam volutpat
+            commodo sed egestas egestas. Eu tincidunt tortor aliquam nulla facilisi cras. Orci
+            sagittis eu volutpat odio facilisis mauris sit amet massa.
+          </p>
+          <p>
+            Lectus sit amet est placerat in egestas erat imperdiet sed. Ipsum a arcu cursus vitae
+            congue mauris rhoncus aenean. Eget egestas purus viverra accumsan in nisl. Eu volutpat
+            odio facilisis mauris sit amet massa vitae tortor. Sit amet purus gravida quis blandit
+            turpis cursus in hac. Vitae tortor condimentum lacinia quis.
+          </p>
+        </Layout>
+      </GlobalStyles>
+    )
+  },
+  { notes: notes }
+)
---
+++ stories/notes/layout.md
@@ -0,0 +1,18 @@
+# Layout
+
+The `<Layout />` component wraps every page-level layout. It is responsible for
+bringing in:
+
+- Header
+- Footer
+- SEO
+- Third-party scripts, including analytics
+- Global styles
+
+## Usage
+
+TODO: Document usage...
+
+## Global Styles
+
+TODO: Document global styles...
