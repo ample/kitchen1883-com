df5cc46b4bf477e07219fe6379098eedcd6671c2
Warren Harrison <warren@hungry-media.com>
2020-01-13 12:12:14 -0500

    Add HelloBar to Layout as a component

---
+++ src/components/layout/hellobar.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const HelloBarScript = () => {
+  return (
+    <script
+      type="text/javascript"
+      src="https://my.hellobar.com/08f4d71ab706322e4241e76574823c3326295e33.js"
+      async
+    ></script>
+  )
+}
+
+export default HelloBarScript
